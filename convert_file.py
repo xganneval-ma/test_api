30ba32effb94815302ba0eed15525b4ce14e4860
xganneval-ma <xganneval@meilleursagents.com>
2021-09-08 15:06:40 +0200

    refactor

--- convert_file.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open("firstnames.csv", "r") as source_file:
-    data = source_file.read().split('\n')
-
-firstnames = list({line.split(";")[0].split('(')[0] for line in data if line})
-with open("firstnames.py", "w") as dest_file:
-    dest_file.write("FIRSTNAMES = %s" % str(firstnames))
--- test/test_workload.py
+++ test/test_workload.py
@@ -1,21 +1,19 @@
 import aiohttp
-import random
 import asyncio
 import datetime
 
 SENARIOS = [
     [[100, 100]] * 5,
-    # [[80, 50]] * 5,
-    # [[50, 70]] * 5,
+    [[80, 50 ]] * 100,
+    [[50, 70 ]] * 100,
 ]
 
 URL = "http://localhost:9518/firstname?waiting_time=%s&count=%s"
 CONCURENCY = 100
 
 
-async def get_data(waiting_time: int, count: int):
+async def get_persons(waiting_time: int, count: int):
     async with aiohttp.ClientSession() as session:
-        print(URL % (waiting_time, count))
         async with session.get(URL % (waiting_time, count)) as resp:
             return await resp.json()
 
@@ -23,10 +21,9 @@ async def get_data(waiting_time: int, count: int):
 async def execute_senarios():
     start_time = datetime.datetime.now()
     for senario in SENARIOS:
-        print("Starting %s" % senario)
-        results = await asyncio.gather(
+        await asyncio.gather(
             *[
-                get_data(senari[0], senari[1])
+                get_persons(senari[0], senari[1])
                 for senari in senario
             ]
         )
